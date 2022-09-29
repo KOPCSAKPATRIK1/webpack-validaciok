8268f1b430c5b6c111b85a2a005d4ac125f6c781
Kopcsak Patrik <kopcsakpatrk1@gmail.com>
2022-09-29 13:23:51 +0200

    kezdes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
+
+import './style.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './style.css';
 
+function toggleMode()
+{
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
+
+function init()
+{
+    console.log("Loaded");
+    document.getElementById('url').addEventListener('input',ChangeKep);
+    document.getElementById('width').addEventListener('change',width);
+    document.getElementById('border').addEventListener('change',borderWidth);
+    document.getElementById('color').addEventListener('input',borderColor);
+    document.getElementById('toggleMode').addEventListener('click', toggleMode)
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
